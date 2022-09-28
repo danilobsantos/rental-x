4db45afce5ff95c5e7223a8555f4d50260293e18
Danilo Santos <danilo@devstudio.com.br>
2022-09-28 19:37:50 -0300

    trabalhando com a tabela Users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+// import users.routes
+import { usersRouters } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
+// adicionando nova rota
+router.use("/users", usersRouters);
 
 export { router };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,18 +1,24 @@
 import { container } from "tsyringe";
 
+// import UsersRepository
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUserRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRespository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
-// passar a interface ICategoryRepository e nomear o registro.
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
     CategoriesRepository
 );
 
-// refatorando controller
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRespository",
     SpecificationsRespository
+);
+// criando injection UsersRepository
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 );
